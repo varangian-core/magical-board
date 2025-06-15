adf6640a0f226270c7e48254a6c7585050a85b65
varangian-core <mikhaltsevichv@gmail.com>
2025-06-14 18:24:11 -0700

    image loader styling

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 export default nextConfig
